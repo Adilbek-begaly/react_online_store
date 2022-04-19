6031180459b22130ae255134f5897a2f66ac3dbb
Adilbek-begaly <Adilbek-begaly@mail.ru>
2022-04-19 10:45:58 +0600

    inital

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const arr = [
+  {title:'Мужские Кроссовки Nike Biazer Mid Suede 1', price:12999,imageUrl:'/img/sneakers/1.jpg', id:1},
+  {title:'Мужские Кроссовки Nike Biazer Mid Suede 2', price:12999,imageUrl:'/img/sneakers/2.jpg', id:2},
+  {title:'Мужские Кроссовки Nike Biazer Mid Suede 3', price:12999,imageUrl:'/img/sneakers/3.jpg', id:3},
+  {title:'Мужские Кроссовки Nike Biazer Mid Suede 4', price:12999,imageUrl:'/img/sneakers/4.jpg', id:4}
+]
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,10 +23,8 @@ function App() {
             <input placeholder="Поиск..."/>
           </div>
         </div>
-       
         <div className="cneakers">
-          <Card/>
-          
+          {arr.map(obj => <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} key={obj.id} onClick={() =>console.log(obj)}/>)}
         </div>
       </div>
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import './Card.module.scss'
 
-function Card() {
+function Card(props) {
+
+  const onClickButton = () => {
+    alert(props.title)
+  }
+
     return (
         <div className="card">
           <div className="favorite">
             <img src="/img/heart.svg" alt="Unliked"/>
           </div>
-          <img width={133} height={122} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-          <h5>Мужские Кроссовки Nike Biazer Mid Suede</h5>
+          <img width={133} height={122} src={props.imageUrl} alt="Sneakers"/>
+          <h5>{props.title}</h5>
           <div className="card-btn">
               <div className="card-info">
                 <span>Цена:</span>
-                <b>12 999 тенге</b>
+                <b>{props.price} тенге</b>
               </div>
-              <button className="button">
+              <button className="button"onClick={props.onClick}>
                 <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
               </button>
             </div>
